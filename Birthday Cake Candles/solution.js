0e4187538f1fdf9bf73df35de615cf3339d50b05
eapenzacharias <eapenzac@gmail.com>
2021-12-14 15:31:40 +0100

    Birthday Cake Candles Sollutiojn added

---
+++ Birthday Cake Candles/solution.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+  inputString = inputString.split('\n');
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles(candles) {
+  // Write your code here
+  let tallest = 0
+  const count = {}
+  candles.forEach((num) => {
+    count[num] = count[num] ? count[num] + 1 : 1;
+    if (tallest < num) {
+      tallest = num;
+    }
+  })
+  return (count[tallest])
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const candlesCount = parseInt(readLine().trim(), 10);
+
+  const candles = readLine().replace(/\s+$/g, '').split(' ').map(candlesTemp => parseInt(candlesTemp, 10));
+
+  const result = birthdayCakeCandles(candles);
+
+  ws.write(result + '\n');
+
+  ws.end();
+}
